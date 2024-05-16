6ffc6045c27d2509612bb39cb2e3736f0b68d377
amine <aminenachit98@gmail.com>
2024-05-16 15:33:16 +0200

    Updates

---
+++ app.js
@@ -0,0 +1,32 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const http_errors_1 = __importDefault(require("http-errors"));
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const path_1 = __importDefault(require("path"));
+const cookie_parser_1 = __importDefault(require("cookie-parser"));
+const morgan_1 = __importDefault(require("morgan"));
+const users_1 = __importDefault(require("./routes/users"));
+const dummy_1 = __importDefault(require("./routes/dummy"));
+const error_handler_1 = __importDefault(require("./error-handler"));
+const real_1 = __importDefault(require("./routes/real"));
+const app = (0, express_1.default)();
+app.use((0, cors_1.default)());
+app.use((0, morgan_1.default)('dev'));
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: false }));
+app.use((0, cookie_parser_1.default)());
+app.use(express_1.default.static(path_1.default.join(__dirname, 'public')));
+app.use('/users', users_1.default);
+app.use('/dummy', dummy_1.default);
+app.use('/real', real_1.default);
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    next((0, http_errors_1.default)(404));
+});
+// error handler
+app.use(error_handler_1.default);
+exports.default = app;
